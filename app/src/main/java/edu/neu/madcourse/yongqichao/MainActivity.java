546ecdfd234c4560abbbb73a7858c9acb17ba349
Yongqi Chao <Jay@Yongqis-MacBook-Pro.local>
2017-01-30 15:30:27 -0800

    add landscape view

--- app/src/main/java/edu/neu/madcourse/yongqichao/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/yongqichao/MainActivity.java
@@ -4,27 +4,16 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout background;
-    Button errorButton, quitButton;
-
-    public  Button aboutButton;
-
-    public void init(){
-        aboutButton = (Button) findViewById(R.id.aboutbutton);
-        aboutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent about = new Intent(MainActivity.this,AboutActivity.class);
-                startActivity(about);
-            }
-        });
-    }
+    Button errorButton, aboutButton, gameButton, quitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         background = (RelativeLayout) findViewById(R.id.activity_main);
-        errorButton = (Button) findViewById(R.id.errorbutton);
-        quitButton = (Button) findViewById(R.id.quitbutton);
 
-        errorButton.setOnClickListener(new View.OnClickListener(){
+
+        //add ABOUT button.
+        aboutButton = (Button)findViewById(R.id.about);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent about = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(about);
+            }
+        });
+
+
+        //add ERROR button
+        errorButton = (Button)findViewById(R.id.error);
+        errorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 background.setBackgroundColor(Color.parseColor("#006699we231"));
             }
         });
 
+
+        //add QUIT button.
+        quitButton = (Button)findViewById(R.id.quit);
         quitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        init();
+
     }
 }
+
