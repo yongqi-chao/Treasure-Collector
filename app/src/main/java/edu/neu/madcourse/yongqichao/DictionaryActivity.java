56006ecb99291c805a3155f057507d4c287ec46a
Yongqi Chao <Jay@Yongqis-MacBook-Pro.local>
2017-01-30 18:01:31 -0800

    add text field into DictionaryActivity

--- app/src/main/java/edu/neu/madcourse/yongqichao/DictionaryActivity.java
+++ app/src/main/java/edu/neu/madcourse/yongqichao/DictionaryActivity.java
@@ -2,12 +2,21 @@ package edu.neu.madcourse.yongqichao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class DictionaryActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+        setTitle("Test Dictionary");
+
+        editText = (EditText) findViewById(R.id.editText);
+
+
+
     }
 }
