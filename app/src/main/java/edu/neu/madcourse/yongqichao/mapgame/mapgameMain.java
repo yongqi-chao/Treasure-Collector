6bb7530250bafb444be40e6933ec4632c21078a8
Yongqi Chao <Jay@Yongqis-MacBook-Pro.local>
2017-04-03 18:00:57 -0700

    add map game layout and  main

---
+++ app/src/main/java/edu/neu/madcourse/yongqichao/mapgame/mapgameMain.java
@@ -0,0 +1,16 @@
+package edu.neu.madcourse.yongqichao.mapgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.neu.madcourse.yongqichao.R;
+
+public class mapgameMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapgame_main);
+        setTitle("Map Game");
+    }
+}
