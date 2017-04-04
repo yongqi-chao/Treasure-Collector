15fb05a5a9887219d5a71703161cfc65aa8d90c5
Yongqi Chao <Jay@Yongqis-MacBook-Pro.local>
2017-04-03 19:35:40 -0700

    add google map

--- app/src/main/java/edu/neu/madcourse/yongqichao/mapgame/mapgameMain.java
+++ app/src/main/java/edu/neu/madcourse/yongqichao/mapgame/mapgameMain.java
@@ -1,8 +1,11 @@
 package edu.neu.madcourse.yongqichao.mapgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import edu.neu.madcourse.yongqichao.AboutActivity;
+import edu.neu.madcourse.yongqichao.MainActivity;
 import edu.neu.madcourse.yongqichao.R;
 
 public class mapgameMain extends AppCompatActivity {
@@ -12,5 +15,8 @@ public class mapgameMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapgame_main);
         setTitle("Map Game");
+
+        Intent map = new Intent(mapgameMain.this, mapgameMap.class);
+        startActivity(map);
     }
 }
